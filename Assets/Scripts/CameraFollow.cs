d3643b55adbee5dc34dc2a7ce2982a8d1d9a8ce9
Weezory <isaac.camiller00@outlook.com>
2025-02-27 15:35:42 +0100

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float transitionSpeed = 10;
+    
+    private Vector3 offset;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //calculates how far the camera is from the target (Player)
+        offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Where cam should go
+        //Lerp - Linear Interpolation (Smoothens movement?)
+        //Move Towards
+        Vector3 targetPosition = target.position - offset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, transitionSpeed * Time.deltaTime);
+
+    }
+}
