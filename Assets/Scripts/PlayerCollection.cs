02f69442f3360de95bdba44aad97853a1039851e
Weezory <isaac.camiller00@outlook.com>
2025-03-10 15:07:41 +0100

    Score Count

--- Assets/Scripts/PlayerCollection.cs
+++ Assets/Scripts/PlayerCollection.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerCollection : MonoBehaviour
 {
     // Add a Scoring System here (HW)
+    public int points;
+    public TMP_Text scoreText;
+
+    public void Score(int value)
+
+    {
+        points = points + value;
+        scoreText.text = $"<b>Score</b> {points}";
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         // Only destroy if collectable
         if (other.CompareTag("Collectable"))
         {
             Destroy(other.gameObject);
+            Score(1);
         }
     }
 }
